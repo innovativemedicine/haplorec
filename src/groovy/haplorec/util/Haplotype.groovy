7f32f7cb2b3ba8806befbd499e1aa3911d22fd93
James Gleeson <james@innovativemedicine.ca>
2013-03-28 16:59:20 -0400

    code up mapping functions for different stages in the pipeline (snp -> [genotype|phenotype] -> drug recommendations) using mysql set-containment queries

--- src/groovy/haplorec/util/Haplotype.groovy
+++ src/groovy/haplorec/util/Haplotype.groovy
@@ -3,62 +3,99 @@ package haplorec.util;
 import groovy.sql.Sql
 
 public class Haplotype {
+    private static def DEFAULT_SAVE_AS = 'query'
+
 	static def snpsToHaplotypes(String url, String username, String password, String driver = "com.mysql.jdbc.Driver") {
 		return snpsToHaplotypes(Sql.newInstance(url, username, password, driver))
 	}
-	
-	static def snpsToHaplotypes(Sql sql) {
-		
+
+	static def snpsToHaplotypes(Sql sql, saveAs = DEFAULT_SAVE_AS, inputSnps = 'input_snps', intoTable = 'input_gene_haplotype') {
+        return selectWhereSetContains(
+            sql,
+            inputSnps,
+            'genotype_drug_recommendation',
+            ['gene_name', 'haplotype_name1', 'haplotype_name2'],
+            ['drug_recommendation_id'],
+            saveAs, 
+            intoTable,
+        )
 	}
 
     // inputGenePhenotype = create table(gene_name, phenotype_name, index(gene_name, phenotype_name))
-    static def genePhenotypeToDrugRecommendation(Sql sql, inputGenePhenotype, saveAs) {
+	static def genePhenotypeToDrugRecommendation(Sql sql, saveAs = DEFAULT_SAVE_AS, inputGenePhenotype = 'input_gene_phenotype', intoTable = 'input_drug_recommendation') {
+        return selectWhereSetContains(
+            sql,
+            inputGenePhenotype,
+            'gene_phenotype_drug_recommendation',
+            ['gene_name', 'phenotype_name'],
+            ['drug_recommendation_id'],
+            saveAs, 
+            intoTable,
+        )
+	}
+
+    static def snpToGeneHaplotype(Sql sql, saveAs = DEFAULT_SAVE_AS, inputVariant = 'input_variant', intoTable = 'input_gene_haplotype') {
+        return selectWhereSetContains(
+            sql,
+            inputVariant,
+            'gene_haplotype_variant',
+            ['snp_id', 'allele'],
+            ['gene_name', 'haplotype_name'],
+            'query', 
+            intoTable,
+        )
     }
 
-    static def snpToGeneHaplotype(Sql sql, inputVariants, saveAs) {
+    static def genotypeToDrugRecommendation(Sql sql, saveAs = DEFAULT_SAVE_AS, inputGenotype = 'input_genotype', intoTable = 'input_drug_recommendation') {
+        return selectWhereSetContains(
+            sql,
+            inputGenotype,
+            'genotype_drug_recommendation',
+            ['gene_name', 'haplotype_name1', 'haplotype_name2'],
+            ['drug_recommendation_id'],
+            'query', 
+            intoTable,
+        )
     }
 
-    static def genotypeToDrugRecommendation(Sql sql, inputGenotype, saveAs) {
-    }
-
-    private static def engines = ['MEMORY', 'MyISAM']
-
-    private static def whereEitherSetContains(queryA, queryB, columns) {
+    private static def whereEitherSetContains(queryA, queryB, setColumns) {
         // refer to src/sql/mysql/subset_query.sql 
-        def columnStr = columns.join(', ')
+        def setColumnsStr = setColumns.join(', ')
         return """\
         ( 
             select count(*) from (
-                select * from ($queryA) A join ($queryB) B using ($columnStr)
+                select * from ($queryA) A join ($queryB) B using ($setColumnsStr)
             ) tmp ) = least(($queryA), ($queryB))
         )
         """
     }
 
     // saveAs = (MEMORY|MyISAM|query|existing)
-    private static def selectWhereSetContains(Sql sql, singleSubsetTable, multiSubsetTable, columns, saveAs, intoTable) {
-        def columnStr = columns.join(', ')
+    private static def selectWhereSetContains(Sql sql, singlesetTable, multisetTable, setColumns, multisetGroupColumns, saveAs, intoTable) {
+        def setColumnsStr = setColumns.join(', ')
+        def multisetGroupColumnsStr = multisetGroupColumns.join(', ')
         def query = { into = '' ->
-            // alias for outer multiSubsetTable of query
+            // alias for outer multisetTable of query
             def outer = "outer"
             def whereEitherSetContainsStr = whereEitherSetContains(
-                "select $columnStr from $singleSubsetTable", 
-                "select $columnStr from $multiSubsetTable where " +
-                    columns.collect { "$it = $outer.$it" }.join('and '),
-                columns
+                "select $setColumnsStr from $singlesetTable", 
+                "select $setColumnsStr from $multisetTable where " +
+                    multisetGroupColumns.collect { "$it = $outer.$it" }.join('and '),
+                setColumns
             )
             return """\
-            select distinct $columnStr from $multiSubsetTable $outer
+            select distinct $multisetGroupColumnsStr from $multisetTable $outer
                 $into
                 where $whereEitherSetContainsStr 
             """
         }
-        return selectAs(sql, query, columns, saveAs, intoTable)
+        return selectAs(sql, query, setColumns, saveAs, intoTable)
     }
 
+    private static def engines = ['MEMORY', 'MyISAM']
+    private static def validSaveAs = engines + ['query', 'existing']
     private static def selectAs(Sql sql, Closure query, columns, saveAs = 'query', intoTable = null) {
         def columnStr = columns.join(', ')
-        def validSaveAs = engines + ['query', 'existing']
         if (engines.any { saveAs == it }) {
             def q = query()
             // create the temporary table using the right datatypes

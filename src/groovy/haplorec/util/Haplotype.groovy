2bec3994e36af00acfb96739da196158080a57c9
James Gleeson <james@innovativemedicine.ca>
2013-04-03 17:01:15 -0400

    write unit tests for helper function groupedRowsToColumns

--- src/groovy/haplorec/util/Haplotype.groovy
+++ src/groovy/haplorec/util/Haplotype.groovy
@@ -5,24 +5,102 @@ import groovy.sql.Sql
 public class Haplotype {
     private static def DEFAULT_SAVE_AS = 'query'
 
-	// static def snpsToHaplotypes(String url, String username, String password, String driver = "com.mysql.jdbc.Driver") {
-	// 	return snpsToHaplotypes(Sql.newInstance(url, username, password, driver))
-	// }
+    // static def snpsToHaplotypes(String url, String username, String password, String driver = "com.mysql.jdbc.Driver") {
+    //     return snpsToHaplotypes(Sql.newInstance(url, username, password, driver))
+    // }
 
-	static def snpsToHaplotypes(Sql sql, saveAs = DEFAULT_SAVE_AS, inputSnps = 'input_snps', intoTable = 'input_gene_haplotype') {
-        return selectWhereSetContains(
-            sql,
-            inputSnps,
-            'genotype_drug_recommendation',
-            ['gene_name', 'haplotype_name1', 'haplotype_name2'],
-            ['drug_recommendation_id'],
-            saveAs, 
-            intoTable,
-        )
-	}
+    // TODO: geneHaplotypeToGenotype: { (GeneName, HaplotypeName) } -> { (GeneName, HaplotypeName, HaplotypeName) }
+    // where error if # of HaplotypeName for a GeneName >= 3, (null, HaplotypeName) if # == 1, else (HaplotypeName, HaplotypeName)
+    static def geneHaplotypeToGenotype(Sql sql, saveAs = DEFAULT_SAVE_AS, inputGeneHaplotype = 'input_gene_haplotype', intoTable = 'input_genotype') {
+    }
+
+    private static def invertedMap(Map m) {
+        m.keySet().inject([:]) {
+            map, key ->    
+            def value = m[key]
+            if ( value instanceof java.util.List ) {
+                value.each { v ->
+                    map[v] = key
+                }
+            } else {
+                map[value] = key
+            }
+            map
+        }
+    }
+
+    // columnMap = ['x':'x', 'y':['y1', 'y2']]
+    // A(x, y) B(x, y1, y2)
+    //   1  2    1  2   3
+    //   1  3
+	//http://stackoverflow.com/questions/13708718/groovy-binding-of-keyword-argument
+    private static def groupedRowsToColumns(Map kwargs, Sql sql, rowTable, columnTable, groupBy, columnMap) {
+		//defaults
+		if (kwargs.badGroup == null) { kwargs.badGroup = { r -> } }
+		// sqlInsert == null
+		// orderRowsBy == null
+        if (groupBy instanceof java.lang.CharSequence) {
+            groupBy = [groupBy]
+        }
+		def orderBy = (kwargs.orderRowsBy != null) ? groupBy + kwargs.orderRowsBy : groupBy
+		def maxGroupSize = columnMap.values().grep { it instanceof java.util.List }.collect { it.size() }.max()
+        if (maxGroupSize == null)  {
+            maxGroupSize = 1
+        }
+        def columnTableColumns = columnMap.values().flatten()
+        def columnTableColumnStr = columnTableColumns.join(', ')
+        def insertGroup = { sqlI, g ->
+            sqlI.withBatch("insert into ${columnTable}(${columnTableColumnStr}) values (${(['?'] * columnTableColumns.size()).join(', ')})".toString()) { ps ->
+                if (g.size() > maxGroupSize) {
+                    badGroup(g)
+                } else {
+                    def i = 0
+                    def values = columnTableColumns.inject([:]) { m, k -> m[k] = null; m }
+                    g.each { row ->
+                        row.keySet().each { k ->
+                            if (columnMap[k] instanceof java.util.List && i < columnMap[k].size()) {
+                                values[columnMap[k][i]] = row[k]
+                            } else if (i == 0 && !(columnMap[k] instanceof java.util.List)) {
+                                values[columnMap[k]] = row[k]
+                            }
+                        }
+						i += 1
+                    }
+                    ps.addBatch(columnTableColumns.collect { values[it] })
+                }
+            }
+        }
+        def lastRowGroup = null
+        List groups = []
+        List group = []
+		def rowCols = columnMap.keySet()
+        sql.eachRow("select * from ${rowTable} order by ${orderBy.join(', ')}".toString()) { row ->
+            def nextRowGroup = groupBy.collect { row[it] }
+            if (lastRowGroup == null) {
+                lastRowGroup = nextRowGroup
+                group.add(rowCols.inject([:]) { m, c -> m[c] = row[c]; m })
+            } else if (lastRowGroup == nextRowGroup) {
+				group.add(rowCols.inject([:]) { m, c -> m[c] = row[c]; m })
+            } else {
+                // process a group
+                if (sqlInsert == null) {
+                    groups.add(group)
+                } else {
+                    insertGroup(sqlInsert, group)
+                    group = []
+                }
+            }
+        }
+        if (group.size() != 0) {
+            groups.add(group)
+        }
+        groups.each { g ->
+            insertGroup(sql, g)
+        }
+    }
 
     // inputGenePhenotype = create table(gene_name, phenotype_name, index(gene_name, phenotype_name))
-	static def genePhenotypeToDrugRecommendation(Sql sql, saveAs = DEFAULT_SAVE_AS, inputGenePhenotype = 'input_gene_phenotype', intoTable = 'input_drug_recommendation') {
+    static def genePhenotypeToDrugRecommendation(Sql sql, saveAs = DEFAULT_SAVE_AS, inputGenePhenotype = 'input_gene_phenotype', intoTable = 'input_drug_recommendation') {
         return selectWhereSetContains(
             sql,
             inputGenePhenotype,
@@ -32,7 +110,7 @@ public class Haplotype {
             saveAs, 
             intoTable,
         )
-	}
+    }
 
     static def snpToGeneHaplotype(Sql sql, saveAs = DEFAULT_SAVE_AS, inputVariant = 'input_variant', intoTable = 'input_gene_haplotype') {
         return selectWhereSetContains(
--- test/groovy/haplorec/test/util/HaplotypeTest.groovy
+++ test/groovy/haplorec/test/util/HaplotypeTest.groovy
@@ -1,5 +1,7 @@
 package haplorec.test.util
 
+import haplorec.util.Haplotype
+
 import groovy.sql.Sql;
 import groovy.util.GroovyTestCase
 
@@ -21,7 +23,8 @@ public class HaplotypeTest extends GroovyTestCase {
         // groovy.sql.Sql is doing some miserably weird garbage behind the scenes when calling 
         // Sql.execute(GString sql); in particular the error message suggests it's replacing 
         // ${SOMEVAR} with 'blah' (single quotes _included_), hence our explicit toString call.
-        sql.execute "create database ${TEST_DB}".toString()
+		sql.execute "drop database if exists ${TEST_DB}".toString()
+		sql.execute "create database ${TEST_DB}".toString()
         sql.close()
         sql = Sql.newInstance("${TEST_URL}/${TEST_DB}", TEST_USER, TEST_PASSWORD)
     }
@@ -29,9 +32,6 @@ public class HaplotypeTest extends GroovyTestCase {
     void tearDown() {
         sql.execute "drop database ${TEST_DB}".toString()
         sql.close()
-    }
-
-    void testSnpsToHaplotypes() {
     }
 
     void testGenePhenotypeToDrugRecommendation() {
@@ -42,5 +42,77 @@ public class HaplotypeTest extends GroovyTestCase {
 
     void testGenotypeToDrugRecommendation() {
     }
+
+    def groupedRowsToColumnsTest(Map kwargs, ACols, ARows, BCols, BRows, groupBy, columnMap) {
+        try {
+
+            // setup
+			def createTable = { table, cols -> sql.execute "create table ${table}(${cols.collect { c -> c + ' integer'}.join(', ')})".toString() }
+			createTable('A', ACols)
+			createTable('B', BCols)
+            def insertSql = { table, columns, rows ->
+                sql.execute """
+                insert into ${table}(${columns.join(', ')}) values
+                ${rows.collect { '(' + it.join(', ') + ')' }.join(', ')}
+                """.toString()
+            }
+            insertSql('A', ACols, ARows)
+            // test
+            Haplotype.groupedRowsToColumns(sql, 'A', 'B', groupBy, columnMap, orderRowsBy: kwargs.orderRowsBy)
+            assertEquals(BRows, sql.rows('select * from B').collect { r -> BCols.collect { r[it] } })
+        } finally {
+            // teardown
+            sql.execute "drop table A".toString()
+            sql.execute "drop table B".toString()
+        }
+    }
+	
+	def testGroupedRowsToColumns() {
+		def ACols = ['x', 'y']
+		def BCols = ['x', 'y1', 'y2']
+		def groupBy = 'x'
+		def columnMap = ['x':'x', 'y':['y1', 'y2']]
+		groupedRowsToColumnsTest(
+			ACols,
+			[
+				[1, 2],
+				[1, 3],
+			],
+			BCols, 
+			[
+				[1, 2, 3],
+			], groupBy, columnMap)
+		groupedRowsToColumnsTest(
+			ACols,
+			[
+				[1, 2],
+			],
+			BCols,
+			[
+				[1, 2, null],
+			], groupBy, columnMap)
+		groupedRowsToColumnsTest(
+			ACols,
+			[
+				// without orderRowsBy: ['y'], we get [1, 3, 2] for the B row
+				[1, 3],
+				[1, 2],
+			],
+			BCols,
+			[
+				[1, 3, 2],
+			], groupBy, columnMap)
+		groupedRowsToColumnsTest(
+			ACols,
+			[
+				// with orderRowsBy: ['y'], we get [1, 2, 3] for the B row
+				[1, 3],
+				[1, 2],
+			],
+			BCols,
+			[
+				[1, 2, 3],
+			], groupBy, columnMap, orderRowsBy:['y'])
+	}
 
 }

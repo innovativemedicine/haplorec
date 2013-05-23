67d76c884afbf1e310b376c93083a9d78abeb6be
James Gleeson <james@innovativemedicine.ca>
2013-05-22 20:03:43 -0400

    finish refactoring that allows scraping individual pages (via scrapy crawl command) while still composing spiders for crawling

--- src/python/pharmgkb/spiders/Gene.py
+++ src/python/pharmgkb/spiders/Gene.py
@@ -8,9 +8,16 @@ from spiders import as_func
 import itertools
 import re
 import json
+import urlparse
 
-def get_base_url(url):
-        return re.search(r'(.*)/gene/[^/]*', url).group(1)
+def get_domain(url):
+    p = urlparse.urlparse(url)
+    return "{}://{}".format(p.scheme, p.netloc)
+
+def spider_request(spider_class, response, **kwargs):
+    base_url = get_domain(response.url)
+    spider = spider_class(base_url, **kwargs)
+    return spider.request(base_url, spider.parse, **kwargs)
 
 class GeneSpider(BaseSpider):
     name = "Gene"
@@ -40,37 +47,53 @@ class GeneSpider(BaseSpider):
                         allele=allele,
                         )
 
-        base_url = get_base_url(response.url)
+        base_url = get_domain(response.url)
         annotation_ids = hxs.select('//script[contains(@type, "javascript") and contains(text(), "popPickers")]/text()').re(r"popPickers\('#edg(\d+)','\d+'\);")
         for annotation_id in annotation_ids:
-            yield FormRequest(base_url + '/views/ajaxGuidelinePickerData.action', formdata={'annotationId': annotation_id}, callback=as_func(GeneHaplotypePhenotypeSpider, base_url=base_url, annotation_id=annotation_id))
+            yield spider_request(GeneHaplotypePhenotypeSpider, response,
+                    annotation_id=annotation_id)
+
+class FormRequestSpider(object):
+    def request(self, base_url, callback, **kwargs):
+        formdata = {}
+        for name, attr in self.formdata:
+            formdata[name] = kwargs[attr]
+        return FormRequest(base_url + self.url, formdata=formdata, callback=callback)
+        
+    def start_requests(self):
+        return [self.request(self.base_url, self.parse, **self.kwargs)]
+
+    def parse(self, response):
+        return self.parse_form_response(response, **self.kwargs)
 
 def quadratic_formula(a, b, c):
     import math
     return (( -b + math.sqrt(b**2 - 4*a*c) ) / 2*a,
             ( -b - math.sqrt(b**2 - 4*a*c) ) / 2*a)
 
-class GeneHaplotypePhenotypeSpider(BaseSpider):
+class GeneHaplotypePhenotypeSpider(FormRequestSpider, BaseSpider):
     name = "GeneHaplotypes"
+    url = '/views/ajaxGuidelinePickerData.action'
     # allowed_domains = ["pharmgkb.org"]
 
-    def __init__(self, base_url='http://www.pharmgkb.org', start_url='/views/ajaxGuidelinePickerData.action', annotation_id=None):
-        self.start_urls = (base_url + start_url,)
-        self.annotation_id = annotation_id
-
-    def start_requests(self):
-        return [FormRequest(self.start_urls[0], formdata={'annotationId': self.annotation_id}, callback=self.parse)]
+    def __init__(self, base_url='http://www.pharmgkb.org', **kwargs):
+        self.base_url = base_url
+        self.start_urls = (self.base_url + self.url,)
+        self.kwargs = kwargs
+        self.formdata = [
+            ('annotationId', 'annotation_id'),
+        ]
 
     # for each haplotype:
     #     parse 'Phenotype (Genotype)'
-    def parse(self, response):
+    def parse_form_response(self, response, annotation_id=None):
         result = None
         try:
             result = json.loads(response.body)
         except ValueError:
             # empty response.body
             return
-        gene = None
+        gene_name = None
         haplotype_names = []
         haplotype_ids = []
         # maximum number of haplotypes there can be to be able to take all pairs of haplotypes and still be less than GenotypeSpider.max_genotype_requests
@@ -80,53 +103,51 @@ class GeneHaplotypePhenotypeSpider(BaseSpider):
         max_haplotypes = int(max(quadratic_formula(1, -1, -2*GenotypeSpider.max_genotype_requests)))
         for r in result['results']:
             if 'gene' in r: 
-                gene = r['gene']
+                gene_name = r['gene']
             if 'haps' in r:
                 for haplotype in r['haps']:
                     haplotype_names.append(haplotype['name'])
                     haplotype_ids.append(haplotype['value'])
         if len(haplotype_names) <= max_haplotypes:
+            # TODO: select with __replacement__
             for haplotype1, haplotype2 in [sorted(genotype, key=lambda haplotype: haplotype[0]) for genotype in itertools.combinations(itertools.izip(haplotype_names, haplotype_ids), 2)]:
                 haplotype_name1, haplotype_id1 = haplotype1
                 haplotype_name2, haplotype_id2 = haplotype2
-                yield GenotypeSpider.request(get_base_url(response.url), as_func(GenotypeSpider, haplotype_name1=haplotype_name1, haplotype_name2=haplotype_name2, haplotype_id1=haplotype_id1, haplotype_id2=haplotype_id2, gene_name=gene_name, annotation_id=annotation_id), annotation_id, haplotype_id1, haplotype_id2, gene_name)
+                yield spider_request(GenotypeSpider, response, 
+                    haplotype_name1=haplotype_name1,
+                    haplotype_name2=haplotype_name2,
+                    haplotype_id1=haplotype_id1,
+                    haplotype_id2=haplotype_id2,
+                    gene_name=gene_name,
+                    annotation_id=annotation_id)
         else:
+            pass
 
-class GenotypeSpider(BaseSpider):
+class GenotypeSpider(FormRequestSpider, BaseSpider):
     max_genotype_requests = 100
     name = "Genotype"
+    url = '/views/alleleGuidelines.action'
     # allowed_domains = ["pharmgkb.org"]
 
     # -a haplotype_name1="*11" -a haplotype_name2="*11" -a haplotype_id1=PA165971564 -a haplotype_id2=PA165971564 -a gene_name=CYP2D6 -a annotation_id=981483939
-    def __init__(self, base_url='http://www.pharmgkb.org', haplotype_name1=None, haplotype_name2=None, haplotype_id1=None, haplotype_id2=None, gene_name=None, annotation_id=None):
-        # self.start_urls = (base_url + start_url,)
+    def __init__(self, base_url='http://www.pharmgkb.org', **kwargs):
         self.base_url = base_url
-        self.annotation_id = annotation_id
-        self.haplotype_id1 = haplotype_id1
-        self.haplotype_id2 = haplotype_id2
-        self.gene_name = gene_name
-        self.haplotype_name1 = haplotype_name1
-        self.haplotype_name2 = haplotype_name2
+        self.start_urls = (self.base_url + self.url,)
+        self.kwargs = kwargs
+        self.formdata = [
+            ('annotationId', 'annotation_id'),
+            ('allele1', 'haplotype_id1'),
+            ('allele2', 'haplotype_id2'),
+            ('location', 'gene_name'),
+        ]
 
-    def start_requests(self):
-        return [GenotypeSpider.request(base_url, self.parse, self.annotation_id, self.haplotype_id1, self.haplotype_id2, self.gene_name)]
-
-    @classmethod
-    def request(cls, base_url, callback, annotation_id=None, haplotype_id1=None, haplotype_id2=None, gene_name=None):
-        return FormRequest(base_url + '/views/alleleGuidelines.action', 
-            formdata={
-                'annotationId': annotation_id,
-                'allele1': haplotype_id1,
-                'allele2': haplotype_id2,
-                'location': gene_name,
-            }, callback=callback)
-
-    def parse(self, response):
+    def parse_form_response(self, response, haplotype_name1=None, haplotype_name2=None, haplotype_id1=None, haplotype_id2=None, gene_name=None, annotation_id=None):
+        import pdb; pdb.set_trace()
         drug_recommendation = items.drug_recommendation()
         genotype_phenotype = items.genotype_phenotype(
-            haplotype_name1=self.haplotype_name1,
-            haplotype_name2=self.haplotype_name2,
-            gene_name=self.gene_name,
+            haplotype_name1=haplotype_name1,
+            haplotype_name2=haplotype_name2,
+            gene_name=gene_name,
         )
 
         hxs = HtmlXPathSelector(response)
@@ -147,20 +168,20 @@ class GenotypeSpider(BaseSpider):
         }
 
         unused_genotype_data = items.unused_genotype_data(values={}, source={
-            'gene_name': self.gene_name,
-            'annotation_id': self.annotation_id,
-            'haplotype_id1': self.haplotype_id1,
-            'haplotype_id2': self.haplotype_id2,
+            'gene_name': gene_name,
+            'annotation_id': annotation_id,
+            'haplotype_id1': haplotype_id1,
+            'haplotype_id2': haplotype_id2,
         })
         for title, value in title_value.iteritems():
             if type(title) is tuple and title[0] == 'Recommendations':
                 drug_recommendation['classification'] = title[1]               
                 drug_recommendation['recommendation'] = value
-            elif title == 'Phenotype (Genotype)' and self.gene_name not in phenotype_exceptions:
+            elif title == 'Phenotype (Genotype)' and gene_name not in phenotype_exceptions:
                 genotype_phenotype['phenotype_name'] = value
             elif title == 'Implications':
                 drug_recommendation['implications'] = value
-            elif self.gene_name in phenotype_exceptions and title == phenotype_exceptions[self.gene_name]:
+            elif gene_name in phenotype_exceptions and title == phenotype_exceptions[gene_name]:
                 genotype_phenotype['phenotype_name'] = value
             else:
                 unused_genotype_data['values'][title] = value
